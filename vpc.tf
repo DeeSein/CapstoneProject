ccdc648706bd617227ec5fa2f0dbcbc67b8518db
DeeSein <168557105+DeeSein@users.noreply.github.com>
2024-05-22 17:45:51 +0200

    added public subnet

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "dev_vpc" {
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.dev_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
