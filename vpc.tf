ebe435f2b5ff05a2b20e36e7260836b0d4322e6d
David <deeseinmedia@gmail.com>
2024-05-28 18:16:23 +0200

    Updated for more

--- vpc.tf
+++ vpc.tf
@@ -5,9 +5,10 @@ resource "aws_vpc" "dev_vpc" {
   enable_dns_support = true
   
   tags       =  {
-    name     = "deham14"
+    Name     = "VPC-DEHAM14"
   }       
 }
+
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.dev_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -15,6 +16,124 @@ resource "aws_subnet" "public-1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "deham14"
+    Name = "DEHAM14-public-1"
   }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "DEHAM14-privat-1" 
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "DEHAM14-public-2"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "DEHAM14-privat-2" 
+  }
+}
+
+### Create a security group for ssh and http
+resource "aws_security_group" "sg_vpc" {
+  name        = "sg_vpc"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_vpc-DEHAM14"
+  }
+}
+
+
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "IGW-DEHAM14"
+  }
+}
+
+resource "aws_route_table" "RB_Public_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "RTB-DEHAM14-public"
+  }
+}
+
+resource "aws_route_table" "RB_Private_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "RTB-DEHAM14-private"
+  }
+}
+resource "aws_route_table_association" "Public_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-1.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
+}
+
+resource "aws_route_table_association" "Private_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-1.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
+}
+
+resource "aws_route_table_association" "Public_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-2.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
+}
+
+resource "aws_route_table_association" "Private_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-2.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
 }
