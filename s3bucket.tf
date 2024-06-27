749a3027f6b295bee71de0f3f90626e0807de2c5
David <deeseinmedia@gmail.com>
2024-06-27 16:37:57 +0200

    adding testfile a S3 Bucket

--- s3bucket.tf
+++ s3bucket.tf
@@ -8,3 +8,13 @@ resource "aws_s3_bucket" "Davids3" {
     Environment = "Dev"
   }
 }
+
+#create a file with the content "this bucket is for testing" and upload it to the S3 bucket
+locals {
+  content = "this bucket is for testing"
+}
+resource "aws_s3_object" "testfile" {
+  bucket  = aws_s3_bucket.Davids3.id
+  key     = "test.txt"
+  content = local.content
+}
