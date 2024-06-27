e9bffce2bb08f8de565780a4b9d75256e9028750
David <deeseinmedia@gmail.com>
2024-06-27 17:16:31 +0200

    fixing Name Tags

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,64 +1,75 @@
-#set up security groups 
+# Set up security group for SSH access
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name_prefix = "allow-ssh-"
   description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id # Replace with your VPC ID
+
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow SSH"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["your_ip/32"] # Replace with your IP address
   }
+
   tags = {
-    Name = allow_ssh
+    Name = "allow-ssh"
   }
 }
 
-#set up security group ingress for http
+# Set up security group for HTTP access
 resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
+  name_prefix = "allow-http-"
   description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id # Replace with your VPC ID
+
   ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name = allow_http
+    Name = "allow-http"
   }
 }
 
-#set up security group ingress for https
+# Set up security group for HTTPS access
 resource "aws_security_group" "allow_https" {
-  name        = "allow_https"
+  name_prefix = "allow-https-"
   description = "Allow HTTPS inbound traffic"
+  vpc_id      = aws_vpc.main.id # Replace with your VPC ID
+
   ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow HTTPS"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name = allow_https
+    Name = "allow-https"
   }
 }
 
-#set up security group ingress for mysql
+# Set up security group for MySQL access
 resource "aws_security_group" "allow_mysql" {
-  name        = "allow_mysql"
+  name_prefix = "allow-mysql-"
   description = "Allow MySQL inbound traffic"
+  vpc_id      = aws_vpc.main.id # Replace with your VPC ID
+
   ingress {
-    description = "Allow MySQL"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow MySQL"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    cidr_blocks     = ["your_ip/32"] # Replace with your IP address
   }
+
   tags = {
-    Name = allow_mysql
+    Name = "allow-mysql"
   }
 }
-
